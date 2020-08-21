32e4c915d6e00bfca351fccae8d67c0ce12b8ee9
NooTheLee <68116656+NooTheLee@users.noreply.github.com>
2020-08-21 10:44:10 +0700

    Add files via upload

---
+++ hh.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct hoahoc
+{
+    char kyhieu[5];
+    int hoatri;
+};
+int BCNN(int a,int b)
+{
+    for(int i=2;i<=a*b;i++)
+    {
+        if(i%a == 0 && i%b == 0)
+        {
+            return i;
+        }
+    }
+}
+void Nhap(hoahoc &ct1, hoahoc &ct2)
+{
+    cout << "Nguyen to 1:" << endl;
+    cout << "Nhap ki hieu: ";
+    fflush(stdin);
+    gets(ct1.kyhieu);
+    cout << "Nhap hoa tri: ";
+    cin >> ct1.hoatri;
+
+    cout << "Nguyen to 2:" << endl;
+    cout << "Nhap ki hieu: ";
+    fflush(stdin);
+    gets(ct2.kyhieu);
+    cout << "Nhap hoa tri: ";
+    cin >> ct2.hoatri;
+}
+void Xuat(hoahoc ct1, hoahoc ct2)
+{
+    int bc = BCNN(ct1.hoatri,ct2.hoatri);
+    cout << ct1.kyhieu << bc/ct1.hoatri << ct2.kyhieu << bc/ct2.hoatri << endl;
+}
+int main()
+{
+    hoahoc ct1,ct2;
+    Nhap(ct1,ct2);
+    Xuat(ct1,ct2);
+}
